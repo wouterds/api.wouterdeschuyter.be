98c991569044c4ff45e6696b8ad5746e7f57b578
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-09 21:52:38 +0200

    :rotating_light: Linting™

--- src/http/index.ts
+++ src/http/index.ts
@@ -5,5 +5,5 @@ const app = express();
 app.get('/', (_req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => {
-  console.log(`> Application is running on http://localhost:3000 🚀`);
+  console.log('> Application is running on http://localhost:3000 🚀');
 });
