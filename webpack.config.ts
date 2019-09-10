6839542968921987dc09d75dc6c1b7bbc77a1b79
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-10 08:47:41 +0200

    :building_construction: Webpack

---
+++ webpack.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import webpack from 'webpack';
+import nodeExternals from 'webpack-node-externals';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/http/index.ts',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  externals: [ nodeExternals() ],
+};
+
+export default config;
