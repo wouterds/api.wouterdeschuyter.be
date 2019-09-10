7338211fb69cfaf414e4b65ae3c11199c7aae3c8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-10 21:27:56 +0200

    :truck: src/services/database.ts -> src/services/sequelize.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from 'sequelize';
-import sequelize from 'services/database';
+import sequelize from 'services/sequelize';
 
 export interface Definition {
   id: string;
