a31fb27aeb49befdc078338f9b2a0028e8c4e277
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-10 22:55:36 +0200

    :alembic: Hello world graphql

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+    type Query {
+      hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
-
-app.get('/', (_req, res) => res.send('Hello World!'));
+server.applyMiddleware({ app });
 
 app.listen(3000, () => {
-  console.log('> Application is running on http://localhost:3000 🚀');
+  console.log(`> Application is running on http://localhost:3000${server.graphqlPath} 🚀`);
 });
