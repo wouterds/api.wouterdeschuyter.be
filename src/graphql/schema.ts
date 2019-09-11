06316d616547877cff273e08060e2b12c0227139
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-11 21:38:13 +0200

    :sparkles: Aggregated schemas

---
+++ src/graphql/schema.ts
@@ -0,0 +1,8 @@
+import path from 'path';
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+
+export default mergeTypes(
+  fileLoader(
+    path.join(__dirname, '**/*.graphql')
+  )
+);
