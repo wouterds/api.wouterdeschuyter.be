0fa33b376c07ce0109ae17c11ca2511062cdb790
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-11 21:48:04 +0200

    :sparkles: Post mutations

---
+++ src/graphql/post/mutation.ts
@@ -0,0 +1,20 @@
+import Post from 'models/post';
+
+type Args = {
+  userId: string,
+  mediaId: string,
+  title: string,
+  slug: string,
+  excerpt: string,
+  body: string,
+};
+
+const addPost = (_parent: any, args: Args) => {
+  const { userId, mediaId, title, slug, excerpt, body } = args;
+
+  return Post.create({ userId, mediaId, title, slug, excerpt, body })
+};
+
+export default {
+  addPost
+};
