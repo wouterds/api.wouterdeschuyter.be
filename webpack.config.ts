440a22b8c58b96d22d851863b783d258a7462987
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-11 23:13:35 +0200

    :wrench: Load ts modules

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,6 +11,18 @@ const config: Configuration = {
     path: path.resolve(__dirname, './dist'),
   },
   externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }],
+      },
+    ],
+  },
 };
 
 export default config;
