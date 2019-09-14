79f098035045f79841fb6a98b326cf326184fe45
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-14 10:25:01 +0200

    :construction: Sensor resolver

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,10 +1,13 @@
 import Post from './post';
+import Sensor from './sensor';
 
 export default {
   Query: {
     ...Post.Query,
+    ...Sensor.Query,
   },
   Mutation: {
     ...Post.Mutation,
+    ...Sensor.Mutation,
   },
 };
