391d84bc7cc50f8c1d55cd22f8c162174121240c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-14 10:23:46 +0200

    :sparkles: Link Sensor schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,4 +1,5 @@
 import { mergeTypes } from 'merge-graphql-schemas';
 import Post from './post/schema.graphql';
+import Sensor from './sensor/schema.graphql';
 
-export default mergeTypes([Post]);
+export default mergeTypes([Post, Sensor]);
