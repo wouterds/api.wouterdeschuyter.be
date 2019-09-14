481398eb7c45c931523bf6d4ffccc91b598798ce
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-14 10:27:49 +0200

    :sparkles: Fetch sensor data

--- src/graphql/sensor/query.ts
+++ src/graphql/sensor/query.ts
@@ -1,1 +1,22 @@
+import axios from 'axios';
+
+const fetchData = async (): Promise<Array<{ id: string; value: number }>> => {
+  const data: any = {};
+
+  try {
+    const { data: response } = await axios.get(
+      `${process.env.ENDPOINT_SENSORS}`
+    );
+
+    data.temperature = response.temperature;
+    data.humidity = response.humidity;
+    data.pressure = response.pressure;
+    data['illuminance:ir'] = response.illuminance.ir;
+    data['illuminance:visible'] = response.illuminance.visible;
+    data['illuminance:full'] = response.illuminance.full;
+  } catch {}
+
+  return data;
+};
+
 export default {};
