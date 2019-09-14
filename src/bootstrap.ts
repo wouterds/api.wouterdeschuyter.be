d347d79913831fec31be9e92a6c18cb1eb42d347
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-14 21:49:09 +0200

    :fire: Remove tsconfig-paths bootstrap

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,3 @@
 import { config as dotenv } from 'dotenv';
-import { register as tsconfigPaths } from 'tsconfig-paths';
-import tsconfig from './../tsconfig.json';
 
 dotenv();
-
-tsconfigPaths({
-  baseUrl: tsconfig.compilerOptions.baseUrl,
-  paths: tsconfig.compilerOptions.paths,
-});
