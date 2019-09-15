f698026b3a73c794bf6601d5a0d86c5a6e969051
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 10:29:38 +0200

    :art: More meaningful variables

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,27 @@
-import express from 'express';
+import Express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './graphql/schema';
 
-const server = new ApolloServer({
+// Server
+const express = Express();
+const apollo = new ApolloServer({
   schema,
   playground: process.env.NODE_ENV !== 'production',
 });
 
-const app = express();
+// Middleware
+express.use(bodyParser.json());
+apollo.applyMiddleware({ app: express });
 
-app.use(bodyParser.json());
-server.applyMiddleware({ app });
-
-// Hello world on root
-app.get('/', (_req, res) =>
+// Hello world
+express.get('/', (_req, res) =>
   res.send(`Hello world! ${new Date().toISOString()}`)
 );
 
-app.listen(3000, () => {
+// Start server on port 3000
+express.listen(3000, () => {
   console.log(
-    `> Application is running on http://localhost:3000${server.graphqlPath} 🚀`
+    `> Application is running on http://localhost:3000${apollo.graphqlPath} 🚀`
   );
 });
