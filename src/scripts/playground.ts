8baaacd3f0f9f360f412fcc8fc8e79267cf5fb2e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 16:03:50 +0200

    :alembic: Testing password service

--- src/scripts/playground.ts
+++ src/scripts/playground.ts
@@ -1,3 +1,15 @@
+import { generateSalt, hashPassword } from 'services/password';
+
 console.log('---- test script ----');
+
+const user = { email: 'wouter.de.schuyter@gmail.com', password: 'azerty123' };
+const salt = generateSalt(user.email);
+const hashedPassword = hashPassword(user.password, salt);
+
+console.log({
+  ...user,
+  salt,
+  hashedPassword,
+});
 
 process.exit(0);
