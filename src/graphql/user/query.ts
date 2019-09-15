b487fda250c5a48a1a563bb02f4b59eb29e66c9d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 20:06:24 +0200

    :sparkles: me query

--- src/graphql/user/query.ts
+++ src/graphql/user/query.ts
@@ -1,1 +1,19 @@
-export default {};
+import User from 'models/user';
+
+const me = async (
+  _parent: any,
+  _args: any,
+  context: { user?: { id: string } }
+) => {
+  const { user } = context;
+
+  if (!user) {
+    throw new Error('unauthorized');
+  }
+
+  return User.findOne({ where: { id: user.id } });
+};
+
+export default {
+  me,
+};
