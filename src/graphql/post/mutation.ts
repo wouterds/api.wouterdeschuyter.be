6f78120a9963fa2650205ba1a5636c43aea5dcd7
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 21:22:40 +0200

    :lock: Protect addPost

--- src/graphql/post/mutation.ts
+++ src/graphql/post/mutation.ts
@@ -9,8 +9,15 @@ const addPost = (
     slug: string;
     excerpt: string;
     body: string;
-  }
+  },
+  context: { user?: { id: string } }
 ) => {
+  const { user } = context;
+
+  if (!user) {
+    throw new Error('unauthorized');
+  }
+
   const { userId, mediaId, title, slug, excerpt, body } = args;
 
   return Post.create({ userId, mediaId, title, slug, excerpt, body });
