2e68fc70a7054a8cb556d9748459bd58ad6d64a1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 18:31:31 +0200

    :sparkles: Healthcheck script

---
+++ src/scripts/healthcheck.ts
@@ -0,0 +1,19 @@
+import { request } from 'http';
+
+const options = {
+  host: 'localhost',
+  port: '3000',
+  path: '/ping',
+};
+
+const req = request(options, ({ statusCode }) => {
+  if (statusCode !== 200) {
+    process.exit(1);
+    return;
+  }
+
+  process.exit(0);
+});
+
+req.on('error', () => process.exit(1));
+req.end();
