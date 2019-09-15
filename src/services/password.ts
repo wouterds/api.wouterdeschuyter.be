1094ffe5cea91ed4e5a38425dcc20e6881bda58e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 15:30:13 +0200

    :sparkles: Password hash generator

--- src/services/password.ts
+++ src/services/password.ts
@@ -5,3 +5,15 @@ export const generateSalt = (salt?: string): string => {
     .createHmac('sha256', `${salt}${Math.random()}${new Date().toISOString()}`)
     .digest('hex');
 };
+
+export const hashPassword = (
+  password: string,
+  salt: string,
+  cycles: number = 10
+): string => {
+  for (let i = 0; i < cycles; i++) {
+    password = crypto.createHmac('sha256', `${salt}:${password}`).digest('hex');
+  }
+
+  return password;
+};
