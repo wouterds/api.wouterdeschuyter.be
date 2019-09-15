93f21a5ae02e280b3642d0c82b1168e04f3156f6
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-15 15:47:42 +0200

    :sparkles: signUp mutation

--- src/graphql/user/mutation.ts
+++ src/graphql/user/mutation.ts
@@ -1,1 +1,37 @@
-export default {};
+import jsonwebtoken from 'jsonwebtoken';
+import User from 'models/user';
+import { generateSalt, hashPassword } from 'services/password';
+
+const signUp = async (
+  _: any,
+  args: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  }
+): Promise<string> => {
+  const { firstName, lastName, email, password } = args;
+
+  if ((await User.count({ where: { email } })) > 0) {
+    throw new Error('email already in use');
+  }
+
+  const salt = generateSalt(email);
+  const hashedPassword = hashPassword(password, salt);
+
+  const { id } = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+
+  return jsonwebtoken.sign({ id }, `${process.env.JWT_SECRET}`, {
+    expiresIn: '1m',
+  });
+};
+
+export default {
+  signUp,
+};
