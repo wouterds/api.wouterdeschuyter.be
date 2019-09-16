073494dd8495d0b75fca18dcdfbc76ccb9f80db0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-16 08:39:10 +0200

    :construction: media stubs

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,17 +1,20 @@
 import { mergeSchemas } from 'graphql-tools';
+import media from './media';
 import post from './post';
 import sensor from './sensor';
 import user from './user';
 
 export default mergeSchemas({
-  schemas: [post.schema, sensor.schema, user.schema],
+  schemas: [media.schema, post.schema, sensor.schema, user.schema],
   resolvers: {
     Query: {
+      ...media.resolver.Query,
       ...post.resolver.Query,
       ...sensor.resolver.Query,
       ...user.resolver.Query,
     },
     Mutation: {
+      ...media.resolver.Mutation,
       ...post.resolver.Mutation,
       ...sensor.resolver.Mutation,
       ...user.resolver.Mutation,
