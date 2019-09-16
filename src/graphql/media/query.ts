de363ff2b6bdcd088fee7c959ae209951031e91e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-16 08:40:59 +0200

    :sparkles: Media query

--- src/graphql/media/query.ts
+++ src/graphql/media/query.ts
@@ -1,1 +1,11 @@
-export default {};
+import Media from 'models/media';
+
+const media = (_parent: any, args: { id: string }) => {
+  const { id } = args;
+
+  return Media.findOne({ where: { id } });
+};
+
+export default {
+  media,
+};
