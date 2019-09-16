85d64bd285b18d0d9475d85334e4e94510bcdfe2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-16 09:37:31 +0200

    :truck: Cleaner graphql structure

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,23 +1,9 @@
 import { mergeSchemas } from 'graphql-tools';
-import media from './media';
-import post from './post';
-import sensor from './sensor';
-import user from './user';
+import media from './media/schema';
+import post from './post/schema';
+import sensor from './sensor/schema';
+import user from './user/schema';
 
 export default mergeSchemas({
-  schemas: [media.schema, post.schema, sensor.schema, user.schema],
-  resolvers: {
-    Query: {
-      ...media.resolver.Query,
-      ...post.resolver.Query,
-      ...sensor.resolver.Query,
-      ...user.resolver.Query,
-    },
-    Mutation: {
-      ...media.resolver.Mutation,
-      ...post.resolver.Mutation,
-      ...sensor.resolver.Mutation,
-      ...user.resolver.Mutation,
-    },
-  },
+  schemas: [media, post, sensor, user],
 });
--- src/graphql/sensor/index.ts
+++
@@ -1,7 +0,0 @@
-import schema from './schema.graphql';
-import resolver from './resolver';
-
-export default {
-  schema,
-  resolver,
-};
