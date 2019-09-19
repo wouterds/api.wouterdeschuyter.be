127baa9357c4db20b09d1340bd17aab1dd00e587
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-19 09:06:32 +0200

    :wrench: Turn @typescript-eslint/explicit-function-return-type off

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": "off"
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
   }
 };
