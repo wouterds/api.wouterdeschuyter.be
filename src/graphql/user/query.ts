cfd1f50ab87fcd28e4fdd1ea76bd2a9da695ce3d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-26 13:16:52 +0200

    :sparkles: User list

--- src/graphql/user/query.ts
+++ src/graphql/user/query.ts
@@ -14,6 +14,21 @@ const me = async (
   return User.findOne({ where: { id: user.id } });
 };
 
+const users = async (
+  _parent: any,
+  _args: any,
+  context: { user?: { id: string } },
+) => {
+  const { user } = context;
+
+  if (!user) {
+    throw new Error('not authenticated');
+  }
+
+  return User.findAll({ order: [['createdAt', 'desc']] });
+};
+
 export default {
   me,
+  users,
 };
