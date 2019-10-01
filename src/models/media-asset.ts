00cc0d46f6177f40906082dcda4ae13d8ce317c8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-01 21:38:46 +0200

    :card_file_box: Added width & height fields

--- src/models/media-asset.ts
+++ src/models/media-asset.ts
@@ -17,6 +17,8 @@ MediaAsset.init(
     name: { type: DataTypes.STRING(128), allowNull: false },
     mediaType: { type: DataTypes.STRING(16), allowNull: true },
     size: { type: DataTypes.INTEGER, allowNull: true },
+    width: { type: DataTypes.INTEGER, allowNull: true },
+    height: { type: DataTypes.INTEGER, allowNull: true },
     md5: { type: DataTypes.STRING(32), allowNull: true },
     path: { type: DataTypes.STRING(64), allowNull: true },
     url: { type: DataTypes.STRING(64), allowNull: true },
