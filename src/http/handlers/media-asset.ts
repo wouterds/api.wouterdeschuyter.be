f792139d26d6ca39419a2b73a94296b55f3deb28
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-01 20:48:06 +0200

    :sparkles: Send file as response

--- src/http/handlers/media-asset.ts
+++ src/http/handlers/media-asset.ts
@@ -2,6 +2,7 @@ import { extname } from 'path';
 import { Request, Response } from 'express';
 import statuses from 'statuses';
 import MediaAsset from 'models/media-asset';
+import { getFile } from 'services/storage';
 
 export default async (req: Request, res: Response) => {
   const { id, ext } = req.params;
@@ -13,12 +14,14 @@ export default async (req: Request, res: Response) => {
     return;
   }
 
-  if (`.${ext}` !== extname(mediaAsset.path)) {
+  const { path, size, mediaType } = mediaAsset;
+
+  if (`.${ext}` !== extname(path)) {
     res.sendStatus(statuses('bad request'));
     return;
   }
 
-  console.log({ 'mediaAsset.path': mediaAsset.path, id, ext });
-
-  res.sendStatus(204);
+  res.header('content-type', mediaType);
+  res.header('content-length', size);
+  getFile(path).pipe(res);
 };
