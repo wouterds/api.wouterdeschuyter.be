debecbd2812b859bc49dddafd9859b6fa21f379e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-02 22:57:14 +0200

    :sparkles: Mail service

---
+++ src/services/mail.ts
@@ -0,0 +1,52 @@
+import mailjet from 'node-mailjet';
+
+const instance = mailjet.connect(
+  `${process.env.MAILJET_API_KEY}`,
+  `${process.env.MAILJET_API_SECRET}`,
+);
+
+interface Person {
+  name: string;
+  email: string;
+}
+
+type Receiver = Person;
+type Sender = Person;
+type ReplyTo = Person;
+
+export const sendMail = async (
+  sender: Sender,
+  receiver: Receiver,
+  replyTo: ReplyTo | null = null,
+  subject: string,
+  text: string,
+) => {
+  const message: any = {
+    From: {
+      Name: sender.name,
+      Email: sender.email,
+    },
+    To: {
+      Name: receiver.name,
+      Email: receiver.email,
+    },
+    Subject: subject,
+    TextPart: text,
+  };
+
+  if (replyTo) {
+    message.ReplyTo = {
+      Name: replyTo.name,
+      Email: replyTo.email,
+    };
+  }
+
+  try {
+    await instance.post('send', { version: 'v3.1' }).request({
+      Messages: [message],
+    });
+    return true;
+  } catch {
+    return false;
+  }
+};
