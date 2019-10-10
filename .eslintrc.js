1fa1ec86cc7f038304f68c6d842da0bcb7ce53df
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-10-10 23:13:24 +0200

    :wrench: Change @typescript-eslint/no-unused-vars to error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": "error",
   }
 };
