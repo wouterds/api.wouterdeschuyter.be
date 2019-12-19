55bbf25815f5d97fe938321860d13442af67f31f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-12-19 20:00:03 +0100

    :sparkles: spotifyAuthorize

---
+++ src/services/spotify.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { addSeconds } from 'date-fns';
+
+export const spotifyAuthorize = async (
+  authorizationCode: string,
+  redirectUri: string,
+) => {
+  const { data: response } = await axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
+      ).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    params: {
+      grant_type: 'authorization_code', // eslint-disable-line
+      code: authorizationCode,
+      redirect_uri: redirectUri, // eslint-disable-line
+    },
+  });
+
+  return {
+    accessToken: response.access_token,
+    refreshToken: response.refresh_token,
+    expiresAt: addSeconds(new Date(), response.expires_in),
+  };
+};
