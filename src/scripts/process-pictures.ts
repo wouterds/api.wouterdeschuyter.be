86b0ea6deb7e0890726f203c9a43bd8eb183b220
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-03-14 23:27:09 +0100

    :construction: Temporary process pictures script

---
+++ src/scripts/process-pictures.ts
@@ -0,0 +1,73 @@
+import { v4 as uuidv4 } from 'uuid';
+import { resolve, join, extname } from 'path';
+import {
+  readdirSync,
+  statSync,
+  mkdirSync,
+  createReadStream,
+  existsSync,
+  copyFileSync,
+} from 'fs';
+import { imageSize } from 'image-size';
+import hasha from 'hasha';
+
+(async () => {
+  const userId = process.argv[2];
+  const source = resolve(__dirname, process.argv[3]);
+  const target = join(source, './_processed');
+  if (!existsSync(target)) {
+    mkdirSync(target);
+  }
+
+  let query =
+    'INSERT INTO `media-assets` (`id`, `userId`, `name`, `mediaType`, `size`, `width`, `height`, `md5`, `path`, `createdAt`, `updatedAt`) VALUES ';
+
+  let i = 0;
+  for (const file of readdirSync(source)) {
+    const ext = extname(file);
+    if (!['.jpg', '.gif', '.png'].includes(ext)) {
+      continue;
+    }
+
+    let mimeType = 'image/jpeg';
+    if (ext === '.gif') {
+      mimeType = 'image/gif';
+    }
+    if (ext === '.png') {
+      mimeType = 'image/png';
+    }
+
+    const path = join(source, file);
+    const size = statSync(path).size;
+    const { width, height } = imageSize(path);
+    const id = uuidv4();
+    const md5 = await hasha.fromStream(createReadStream(path), {
+      algorithm: 'md5',
+    });
+
+    copyFileSync(path, join(target, `${id}${ext}`));
+
+    if (i++ > 0) {
+      query += ',';
+    }
+
+    query += '(';
+    query += `'${id}',`;
+    query += `'${userId}',`;
+    query += `'${file}',`;
+    query += `'${mimeType}',`;
+    query += `${size},`;
+    query += `${width},`;
+    query += `${height},`;
+    query += `'${md5}',`;
+    query += `'/media-assets/${id}${ext}',`;
+    query += 'NOW(),';
+    query += 'NOW()';
+    query += ')';
+  }
+  query += ';';
+
+  console.log(query);
+
+  process.exit(0);
+})();
