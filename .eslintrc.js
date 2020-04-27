ca228fcd34ee162434e705e2b238799437c654fb
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-27 08:45:16 +0200

    :hammer_and_wrench: Enable simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,14 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
+  plugins: [
+    'simple-import-sort',
+  ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    'simple-import-sort/sort': 'error',
   }
 };
