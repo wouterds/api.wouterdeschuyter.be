c723597e6857e9fe80e868edfb48c3cefadb8c45
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-27 08:52:26 +0200

    :recycle: Get rid of statuses package

--- src/request-handlers/media-asset.ts
+++ src/request-handlers/media-asset.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import MediaAsset from 'models/media-asset';
 import { extname } from 'path';
 import { getFile } from 'services/storage';
-import statuses from 'statuses';
 
 export default async (req: Request, res: Response) => {
   const { id, ext } = req.params;
@@ -10,14 +9,14 @@ export default async (req: Request, res: Response) => {
   const mediaAsset = await MediaAsset.findOne({ where: { id } });
 
   if (!mediaAsset) {
-    res.sendStatus(statuses('not found'));
+    res.sendStatus(404);
     return;
   }
 
   const { path, size, mediaType } = mediaAsset;
 
   if (`.${ext}` !== extname(path)) {
-    res.sendStatus(statuses('bad request'));
+    res.sendStatus(400);
     return;
   }
 
