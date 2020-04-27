df640b1cb3cf650c11ae7d7494ecc9d1ea918264
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-04-27 08:47:31 +0200

    :art: Single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,10 @@ module.exports = {
     'simple-import-sort',
   ],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-vars": "error",
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
     'simple-import-sort/sort': 'error',
   }
 };
