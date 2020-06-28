3ac2517740a1f0dec832bb88f7e1e1109021ec11
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-28 14:58:45 +0200

    :sparkles: Calculate blurhash

--- src/scripts/backfill-blurhash.ts
+++ src/scripts/backfill-blurhash.ts
@@ -1,5 +1,29 @@
+import { encode } from 'blurhash';
 import MediaAsset from 'models/media-asset';
-import path from 'path';
+import { join, resolve } from 'path';
+import sharp from 'sharp';
+
+// https://github.com/lovell/sharp/blob/master/docs/install.md#alpine-linux
+sharp.cache(false);
+
+// https://github.com/woltapp/blurhash/issues/43#issuecomment-597674435
+const encodeImageToBlurhash = (path: string): Promise<string> => {
+  console.log({ path });
+
+  return new Promise((resolve, reject) => {
+    sharp(path)
+      .raw()
+      .ensureAlpha()
+      .resize(32, 32, { fit: 'inside' })
+      .toBuffer((err, buffer, { width, height }) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(encode(new Uint8ClampedArray(buffer), width, height, 4, 4));
+      });
+  });
+};
 
 (async () => {
   for (const mediaAsset of await MediaAsset.findAll()) {
@@ -7,9 +31,11 @@ import path from 'path';
       continue;
     }
 
-    const location = path.join(path.resolve('/data'), mediaAsset.path);
+    const blurhash = await encodeImageToBlurhash(
+      join(resolve('/data'), mediaAsset.path),
+    );
 
-    console.log({ location });
+    console.log({ blurhash });
   }
 
   process.exit(0);
