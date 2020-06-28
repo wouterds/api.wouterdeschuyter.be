dda0f49664c25d74305371ad8c91749e81401b67
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-06-28 14:51:49 +0200

    :sparkles: Loop through assets and determine location

--- src/scripts/backfill-blurhash.ts
+++ src/scripts/backfill-blurhash.ts
@@ -1,3 +1,16 @@
+import MediaAsset from 'models/media-asset';
+import path from 'path';
+
 (async () => {
+  for (const mediaAsset of await MediaAsset.findAll()) {
+    if (!mediaAsset.path) {
+      continue;
+    }
+
+    const location = path.join(path.resolve('/data'), mediaAsset.path);
+
+    console.log({ location });
+  }
+
   process.exit(0);
 })();
